6443b490c07277812e841f2d9873f7f9796182e3
souvik1232 <souvik199831@gmail.com>
2020-11-29 09:15:52 +0530

    Added:EmployeeDetails's object used for storing and displaying data

---
+++ Employee.java
@@ -0,0 +1,14 @@
+package com.java.classconstructor;
+
+public class Employee {
+
+	public static void main(String[] args) {
+
+		
+		Employeedetails obj1 = new Employeedetails(10245,"Chaitanya");  
+		Employeedetails obj2 = new Employeedetails(92232,"Negan");  
+		obj1.info();  
+		obj2.info();
+	}
+
+}
